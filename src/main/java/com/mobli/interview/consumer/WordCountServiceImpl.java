b8b8164684da6db9ac00e984e2809c8ae5dd243a
Eldar Damari <eldar.damari@gmail.com>
2015-02-16 11:47:58 +0200

    totalWords will count number of words, consume is synchronized for thread safe operations

--- src/main/java/com/mobli/interview/consumer/WordCountServiceImpl.java
+++ src/main/java/com/mobli/interview/consumer/WordCountServiceImpl.java
@@ -1,18 +1,29 @@
 package main.java.com.mobli.interview.consumer;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import main.java.com.mobli.interview.data.SmsMessage;
 
 public class WordCountServiceImpl implements WordCountService {
 
+	private Map<String, Integer> totalWords = new HashMap<String, Integer>();
+	
     @Override
     public Map<String, Integer> getTotalWords() {
-        return null;
+        return this.totalWords;
     }
 
     @Override
-    public void consume(SmsMessage message) {
-
+    public synchronized void consume(SmsMessage message) {
+    		
+    	String splited[] = message.getText().split(" ");
+    		
+    		for (String word : splited) {
+    			if (this.totalWords.containsKey(word))
+    				this.totalWords.put(word,this.totalWords.get(word)+1);
+    			else
+    				this.totalWords.put(word,1);
+    		}
     }
 }
