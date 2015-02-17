88ceecffa69c6e15712a6584a6480206afbe2fa6
Eldar Damari <eldar.damari@gmail.com>
2015-02-17 09:49:05 +0200

    EDIT - implemented shutdown methods and class documentation

--- src/main/java/com/mobli/interview/broker/MessageBrokerImpl.java
+++ src/main/java/com/mobli/interview/broker/MessageBrokerImpl.java
@@ -8,29 +8,62 @@ import main.java.com.mobli.interview.data.SmsMessage;
 
 public class MessageBrokerImpl implements MessageBroker {
     
-	private Map<String, SmsConsumer> subscribers = new HashMap<String,SmsConsumer>();
+	private Map<String, SmsConsumer> subscribers;
+    
+    private MessageBrokerImpl() {
+        this.subscribers = new HashMap<String,SmsConsumer>();
+    }
+
+    // Singleton Operation 
+    private static class SingletonHelper{
+        private static final MessageBrokerImpl INSTANCE = new MessageBrokerImpl();
+    }
+     
+    public static MessageBrokerImpl getInstance(){
+        return SingletonHelper.INSTANCE;
+    }
+
 	
+    /**
+     * Add message subscriber to listen and receive all the SMS messages sent to the broker.
+     * @param consumer The consumer that handles the received message.
+     */
 	@Override
     public synchronized void addSubscriber(SmsConsumer consumer) {
 			this.subscribers.put(getClassName(consumer),consumer);
     }
 
+    /**
+     * Remove the subscriber from listening and receiving SMS messages.
+     * @param consumer
+     */
     @Override
     public synchronized void removeSubscriber(SmsConsumer consumer) {
 			this.subscribers.remove(getClassName(consumer),consumer);
     }
 
+    /**
+     * Receive new message from message producer and publish it to all the subscribers.
+     * @param message
+     */
     @Override
     public void receive(SmsMessage message) {
     	for (Map.Entry<String,SmsConsumer> entry : this.subscribers.entrySet())
     		entry.getValue().consume(message);
     }
 
+    /**
+     * Shutdown broker and cleanup all resources.
+     */
     @Override
     public void shutdown() {
-
+        this.subscribers = null;
     }
     
+    /**
+     * Receive class name, used to save as key in subscribers HashMap 
+     * @param c
+     */
     private String getClassName(SmsConsumer c) {
     	return c.getClass().getSimpleName();
     }
